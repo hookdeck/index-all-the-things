f0379917ed6e354ecfb03f9089f74d8c4db00dbf
Phil Leggetter <phil@leggetter.co.uk>
2024-10-04 10:19:15 +0100

    chore: check for existing vector index

--- create-vector-index.py
+++ create-vector-index.py
@@ -6,21 +6,31 @@ from pymongo.operations import SearchIndexModel
 database = Database()
 collection = database.get_collection()
 
-# Create your index model, then create the search index
-search_index_model = SearchIndexModel(
-    definition={
-        "fields": [
-            {
-                "type": "vector",
-                "path": "embedding",
-                "numDimensions": 768,
-                "similarity": "euclidean",
-            }
-        ]
-    },
-    name="vector_index",
-    type="vectorSearch",
-)
+search_index_name = "vector_index"
+search_index_definition = {
+    "fields": [
+        {
+            "type": "vector",
+            "path": "embedding",
+            "numDimensions": 768,
+            "similarity": "euclidean",
+        }
+    ]
+}
 
-result = collection.create_search_index(model=search_index_model)
+if collection.list_search_indexes(search_index_name) is not None:
+    print(f'Creating search index: "{search_index_name}"')
+    search_index_model = SearchIndexModel(
+        definition=search_index_definition,
+        name=search_index_name,
+        type="vectorSearch",
+    )
+    result = collection.create_search_index(model=search_index_model)
+
+else:
+    print(f'Search index "{search_index_name}" already exists. Updating.')
+    result = collection.update_search_index(
+        name=search_index_name, definition=search_index_definition
+    )
+
 print(result)
